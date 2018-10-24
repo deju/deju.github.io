0fee55aca356401b80c617b41b5037874d5b2c25
deju <deju.tu@gmail.com>
2018-10-25 00:32:04 +0800

    feat(react-coming-soon): react-coming-soon demo

---
+++ react-coming-soon/precache-manifest.d71adf996609813464830704be521ec1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "94a4cecc868af9904233",
+    "url": "/static/js/main.94a4cecc.chunk.js"
+  },
+  {
+    "revision": "94c549461de3f8782a6b",
+    "url": "/static/js/1.94c54946.chunk.js"
+  },
+  {
+    "revision": "94a4cecc868af9904233",
+    "url": "/static/css/main.c5f5f5ac.chunk.css"
+  },
+  {
+    "revision": "52d027d8b71760f23181e6d5549f443e",
+    "url": "/index.html"
+  }
+];
